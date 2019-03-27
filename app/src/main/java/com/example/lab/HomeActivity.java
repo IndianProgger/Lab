0566be50d31cf7854539dbf5350c190363330bbf
Fyodor Perevozchikov <31614726+IndianProgger@users.noreply.github.com>
2019-03-27 14:05:15 +0300

    Update HomeActivity.java

--- app/src/main/java/com/example/lab/HomeActivity.java
+++ app/src/main/java/com/example/lab/HomeActivity.java
@@ -2,10 +2,6 @@ package com.example.lab;
 
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 
 public class HomeActivity extends ListActivity {
 
